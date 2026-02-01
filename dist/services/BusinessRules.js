9ac483a10135b8a6296d026e1ad8b905b9918ac2
Tiago Machado <timac1997@gmail.com>
2026-02-01 16:08:16 +0000

    business rules added

--- dist/services/BusinessRules.js
+++ dist/services/BusinessRules.js
@@ -1,1 +1,15 @@
-"use strict";
+export class BusinessRules {
+    constructor() { } // prevents new instances
+    static canUserBeDeactivated(activeTasks) {
+        return activeTasks === 0; // deactivates user when it has 0 tasks
+    }
+    static canTaskBeCompleted(isBlocked) {
+        return !isBlocked; // only allows completion when not blocked (false)
+    }
+    static canAssignTask(active) {
+        return active; // only allows task assignment of active users
+    }
+    static isValidTitle(title) {
+        return title.trim().length >= 3; // only allows valid titles above 3 characters and trims the empty spaces in the string so there are no empty strings
+    }
+}
