89e665daa7e8b5ebe9ca94b52324ae87b157055e
Tiago Machado <timac1997@gmail.com>
2026-02-02 17:55:45 +0000

    pretier fix

--- src/utils/TagManager.ts
+++ src/utils/TagManager.ts
@@ -1,20 +1,19 @@
 export class TagManager<T> {
-
+    
   private tags: Map<T, string[]>;
 
   constructor() {
     this.tags = new Map<T, string[]>();
   }
 
-  addTag(item: T, tag: string): void {    
+  addTag(item: T, tag: string): void {
     const existingTags = this.tags.get(item);
 
-    if (existingTags) {      
+    if (existingTags) {
       if (!existingTags.find((t) => t === tag)) {
         existingTags.push(tag);
       }
     } else {
-      
       this.tags.set(item, [tag]);
     }
   }
@@ -23,7 +22,6 @@ export class TagManager<T> {
     const existingTags = this.tags.get(item);
 
     if (existingTags) {
-      
       const index = existingTags.findIndex((t) => t === tag);
 
       if (index !== -1) {
